137dc06f8ad1be9d7a3cb6617533d9050dcffffb
AbdulRehman817 <abdulrehmanbey1718@gmail.com>
2026-05-08 11:14:02 +0500

    Closure done

---
+++ Scopes/hello.py
@@ -0,0 +1,38 @@
+# username="Chai Aur Code"
+
+# def func():
+    # username='chai'
+    # print(username)
+    
+# print(username)
+# func()
+
+
+# x=99;
+
+# def func2(y):
+#     z=x+y
+#     return z
+
+# a=func2(2);
+# print(a)
+
+
+
+
+# x=99;
+
+# def func3():
+#     x=88
+   
+
+# print(x)
+
+
+x=99
+def f1():
+    # x=66;
+    def f2():
+        print(x)
+    f2()
+f1()
