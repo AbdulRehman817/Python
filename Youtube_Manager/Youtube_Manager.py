abf020e97bc98f928a8419bceb977b3ae2308717
AbdulRehman817 <abdulrehmanbey1718@gmail.com>
2026-05-10 01:01:58 +0500

    Implement YouTube Manager with video management features

---
+++ Youtube_Manager/Youtube_Manager.py
@@ -0,0 +1,62 @@
+import json
+
+def load_data():
+    try:
+        with open("youtube.txt","r") as file:
+           return json.load(file)
+    except FileNotFoundError:
+        return []
+
+def save_data_helper(videos):
+    with open("youtube.txt","w") as file:
+        json.dump(videos,file)
+
+def list_all_videos(videos):
+  for index,video in  enumerate(videos,start=1):  # enumare add indexing in list , tupes and dictories if the these donot have
+     print(f"{index}. {video["name"]} and duration {video["length"]} ")
+def add_video(videos):
+    video_name=input("Enter video name")
+    video_time=input("Enter video length")
+    videos.append({"name":video_name,"length":video_time})
+    save_data_helper(videos)
+def update_video(videos):
+    list_all_videos()
+    index=int(input("Enter the video number to update"))
+    if 1<=index<=len(videos):
+        name=input("Enter the new video name")
+        time=input("Enter the new video time")
+        videos[index-1]={"name":name,"length":time}
+        save_data_helper()
+    else:
+        print("Invalid index")
+    
+def delete_video(videos):
+    pass
+
+
+def main():
+    videos=load_data()
+    while True:
+        print("\n Youtube Manager | Choose an option")
+        print("1. List all videos")
+        print("2. Add a youtube video")
+        print("3. Update a youtube video details")
+        print("4. Delete a youtube video")
+        print("4. Exit the app")
+        choice=input("Enter your choice")
+        match choice:
+            case "1":
+                list_all_videos(videos)
+            case "2":
+                add_video(videos)
+            case "3":
+                update_video(videos)
+            case "4":
+                delete_video(videos)
+            case "5":
+                break;
+            case _:
+                print("Invalid choice")
+            
+if __name__ == "__main__":
+    main()
